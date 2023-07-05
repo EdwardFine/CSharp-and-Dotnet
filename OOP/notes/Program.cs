24ce4e07a6966e7a5202333fcd681aefe1d6f3c6
EdwardFine <edfine03@hotmail.com>
2023-07-05 15:16:03 -0700

    PolyMorphism

--- OOP/notes/Program.cs
+++ OOP/notes/Program.cs
@@ -9,3 +9,12 @@ Console.WriteLine(Giraffe.Type);
 Cat Merida = new Cat("Merida","Orange Tabby");
 
 Merida.ShowInfo();
+
+List<Animal> AllAnimals = new List<Animal> {Giraffe,Merida};
+
+foreach(Animal a in AllAnimals){
+    a.ShowInfo();
+    if(a is Cat){
+        Console.WriteLine("ACHOO!");
+    }
+}
