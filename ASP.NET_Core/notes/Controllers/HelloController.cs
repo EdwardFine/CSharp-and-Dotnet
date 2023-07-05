53d71a2797c12d1771d842e2f6e8c2a98a0aece6
EdwardFine <edfine03@hotmail.com>
2023-07-05 11:53:34 -0700

    First Lecture for asp.net

---
+++ ASP.NET_Core/notes/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+// This brings all the MVC features we need to this file
+using Microsoft.AspNetCore.Mvc;
+// Be sure to use your own project's namespace here! 
+namespace notes.Controllers;
+public class HelloController : Controller   // Remember inheritance?    
+{
+    [HttpGet] // We will go over this in more detail on the next page    
+    [Route("")] // We will go over this in more detail on the next page
+    public ViewResult Index()
+    {
+        ViewBag.Name = "Carlie";
+        return View("Index");
+    }
+    [HttpGet("users/more")]
+    public ViewResult AUser(){
+        return View();
+    }
+}
+
