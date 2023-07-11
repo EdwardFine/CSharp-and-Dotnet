36349f46c74a6b6523a70f04f21d5404cfacf112
EdwardFine <edfine03@hotmail.com>
2023-07-11 09:28:05 -0700

    First Connection

---
+++ LINQ/FirstConnection/Models/PetContext.cs
@@ -0,0 +1,10 @@
+#pragma warning disable CS8618
+
+using Microsoft.EntityFrameworkCore;
+namespace FirstConnection.Models;
+
+public class PetContext : DbContext 
+{   
+    public PetContext(DbContextOptions options) : base(options) { }      
+    public DbSet<Pet> Pets { get; set; } 
+}
